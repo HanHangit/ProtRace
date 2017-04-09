2d2cc1cc2dbc2d4481c8b438056463fba0fd70a8
Chep70 <Simon.Nestrowicz@gmx.de>
2017-04-09 17:17:08 +0200

    Add files via upload

---
+++ ProtRace/Box.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace ProtRace
+{
+    public class Box
+    {
+        Model model;
+        Vector3 pos;
+        float scale;
+        public void Initialize(ContentManager contentManager)
+        {
+            model = contentManager.Load<Model>("box");
+
+        }
+        public Vector3 getPos()
+        {
+            return pos;
+        }
+        public float getScale()
+        {
+            return scale;
+        }
+
+
+        public Box(Vector3 position)
+        {
+            pos = position;
+            scale = 1f;
+        }
+
+        public Box(Vector3 position, float scalefaktor)
+        {
+            pos = position;
+            scale = scalefaktor;
+        }
+
+        public void Draw(Matrix view)
+        {
+    
+
+            //draw box
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.PreferPerPixelLighting = true;
+                  
+                    effect.World = Matrix.CreateScale(scale)*Matrix.CreateTranslation(pos);
+  
+                    effect.View = view;
+
+                    effect.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 1f, 0.1f, 200f);
+
+                }
+
+                mesh.Draw();
+            }
+        }
+    }
+
+}
--- ProtRace/Game1.cs
+++ ProtRace/Game1.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-
+using System.Collections.Generic;
+//X-positiv links
+//Y-positiv oben
+//Z-positiv hinten
 namespace ProtRace
 {
     /// <summary>
@@ -11,10 +14,18 @@ namespace ProtRace
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-
+        Model model;
+        List<Box> boxofboxes = new List<Box>();
+        float rotate; //rotate wheels
+        private Vector3 position;
+        private Matrix world= Matrix.Identity;
+        private Matrix view = Matrix.Identity;
+        private Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 800f / 480f, 0.1f, 500f);
+      
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+                //graphics.IsFullScreen = true;
             Content.RootDirectory = "Content";
         }
 
@@ -27,7 +38,12 @@ namespace ProtRace
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            rotate = 0; 
+            boxofboxes.Add(new Box(new Vector3(0, 1, -20), 2f));
+            boxofboxes.Add(new Box(new Vector3(10, 1,-10 )));
+            boxofboxes.Add(new Box(new Vector3(-10, 1, -10)));
+            foreach (Box box in boxofboxes)
+                box.Initialize(Content);
             base.Initialize();
         }
 
@@ -39,8 +55,10 @@ namespace ProtRace
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            model = Content.Load<Model>("robot");
+          //  box = Content.Load<Model>("box");
             // TODO: use this.Content to load your game content here
+
         }
 
         /// <summary>
@@ -61,8 +79,60 @@ namespace ProtRace
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+          
+                        // TODO: Add your update logic here
+                        KeyboardState state = Keyboard.GetState();
 
-            // TODO: Add your update logic here
+                        if (state.IsKeyDown(Keys.Right))
+                        {
+                           // position += new Vector3(1f, 0, 0);
+                rotate = 0.7f;
+                //world = Matrix.CreateTranslation(1f, 0, 0);
+            }
+
+
+                        else if (state.IsKeyDown(Keys.Left))
+                        {
+                            //position -= new Vector3(1f, 0, 0);
+                  rotate = -0.7f;
+               // world = Matrix.CreateTranslation(-1f, 0, 0);
+            }
+                        else
+                        {
+                rotate = 0;
+                            // if (rotate > 0) rotate = rotate - 0.0075f;
+                            //  if (rotate < 0) rotate = rotate + 0.0075f;
+                        }
+
+                        if (state.IsKeyDown(Keys.Up))
+                        {
+                if(rotate!=0)
+                            position -= new Vector3(-rotate, 0, 0.5f);
+                else
+                    position -= new Vector3(-rotate, 0, 1f);
+                // world = Matrix.CreateTranslation(0, 0, -1f);
+
+            }
+                        if (state.IsKeyDown(Keys.Down))
+                        {
+                if (rotate != 0)
+                    position += new Vector3(-rotate, 0, 0.5f);
+                else
+                    position += new Vector3(-rotate, 0, 1f);
+                //   world = Matrix.CreateTranslation(0, 0, 1f);
+            }
+
+     
+
+            //2.5f*rotate
+            view = Matrix.CreateLookAt(new Vector3(0 + position.X, position.Y +2, position.Z +8), new Vector3(0 + position.X, position.Y , position.Z - 5), Vector3.UnitY);
+ 
+            //if (position.Z==-100f) Exit();
+ 
+            foreach (Box box in boxofboxes)
+                if (System.Math.Abs(box.getPos().X-position.X)<2.05*box.getScale() &&
+       System.Math.Abs(box.getPos().Y - position.Y )< 2.05* box.getScale() &&
+       System.Math.Abs(box.getPos().Z - position.Z )< 2.05* box.getScale()) Exit();
 
             base.Update(gameTime);
         }
@@ -74,10 +144,46 @@ namespace ProtRace
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
             // TODO: Add your drawing code here
+            // A model is composed of "Meshes" which are
+            // parts of the model which can be positioned
+            // independently, which can use different textures,
+            // and which can have different rendering states
+            // such as lighting applied.
+            float aspectRatio =
+           graphics.PreferredBackBufferWidth / (float)graphics.PreferredBackBufferHeight;
+
+ 
+            //draw player
+            DrawModel(model, world, view, projection);
+            //draw box
+            foreach (Box box in boxofboxes)
+                box.Draw(view);
+            
+          
+       
+          
+           // GraphicsDevice.SamplerStates[0] = SamplerState.LinearWrap;
+           // GraphicsDevice.BlendState = BlendState.Opaque;
+           // GraphicsDevice.DepthStencilState = DepthStencilState.Default;
 
             base.Draw(gameTime);
         }
+ 
+        private void DrawModel(Model model, Matrix world, Matrix view, Matrix projection)
+        {
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = effect.World = Matrix.CreateTranslation(position);
+                    effect.View = view;
+                    effect.Projection = projection;
+                }
+
+                mesh.Draw();
+            }
+        }
+
     }
 }
